1f3404e46fd09e7501ae2f23a63872c5b0ab30f3
Tanner Lehett <john.cfplus@gmail.com>
2024-02-04 17:24:46 -0800

    Start working on demos.

--- src/pages/ReactContextRerenderDemo/index.jsx
+++ src/pages/ReactContextRerenderDemo/index.jsx
@@ -1,11 +1,39 @@
+import { useState, memo } from 'react';
 import localStyles from './ReactContextRerenderDemo.module.scss';
 
 function ReactContextRerenderDemo() {
+    console.log('ReactContextRerenderDemo re-rendered');
+
+    const [count, setCount] = useState(0);
+
     return (
         <div className={localStyles.pageContent}>
-            <h1>Context Rerender Demo</h1>
+            <h1>React Context Re-Render Demo</h1>
+
+            <Counter count={count}/>
+            <MemoizedIncrement setCount={setCount}/>
         </div>
-    )
+    );
 }
 
 export default ReactContextRerenderDemo;
+
+function Counter({ count }) {
+    console.log('Counter re-rendered');
+
+    return (
+        <div>Count: {count}</div>
+    );
+}
+
+function Increment({ setCount }) {
+    console.log('Increment re-rendered');
+
+    return (
+        <button type="button" onClick={() => setCount(c => c + 1)}>
+            Increment
+        </button>
+    );
+}
+
+const MemoizedIncrement = memo(Increment);
